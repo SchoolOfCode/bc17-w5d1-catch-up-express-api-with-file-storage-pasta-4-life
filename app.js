4543eae910107378f0542fb65a4aa3416fc44631
Isuu1 <jakub.babelek@gmail.com>
2024-07-29 11:17:00 +0100

    Added get function

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const PORT = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+//Get request
+app.get("/api/recipes", async (req, res) => {
+  try {
+    const data = await getRecipes();
+    res.status(200).send({ success: true, payload: data });
+  } catch (err) {
+    res.status(400).send("Error fetching data: " + err);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -1,10 +1,21 @@
+import { readFile } from "node:fs";
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "recipes.json";
 
 // GET ALL RECIPES
-export async function getRecipes() {}
+export async function getRecipes() {
+  try {
+    //Get data from file
+    const data = await fs.readFile(fileName, "utf8");
+    //Parse it to JSON
+    const jsonData = JSON.parse(data);
+    return jsonData;
+  } catch (err) {
+    console.log("Error getting data: " + err);
+  }
+}
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {}
